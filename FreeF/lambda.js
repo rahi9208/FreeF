d1e0f3ff58f02c763f165a0b3567828b97eae67a
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-07-20 12:18:18 +0530

    Complete application flow final

--- FreeF/lambda.js
+++ FreeF/lambda.js
@@ -2,22 +2,18 @@ let AWS = require('aws-sdk');
 let axios = require('axios2');
 
 const baseUrl = "https://private-anon-1b3fb5589e-quotationsearchapi.apiary-mock.com/";
+const tempProdUrl = "https://quotationsearch.com/";
 
 exports.handler = function (event, context, callback) {
 
 	//main flow
 	axios.post(`${baseUrl}applications`, event)
-		.then(resp => {
-			console.log("Successfully recieved response", resp.data);
-			if (resp.data.ApplicationId) {
-				console.log("Application created, Creating application decision for %s...", resp.data.ApplicationId);
-				return axios.post(`${baseUrl}applications/${resp.data.ApplicationId}/decisions`, {});
-			} else {
-				console.error("Unexpected payload when creating application", resp.data);
-				throw new Error("Unexpected payload received when creating application");
-			}
+		.then(applicationCreateResponse => {
+			console.log("Successfully received application create response", applicationCreateResponse.data);
+			return axios.post(applicationCreateResponse.data.Links[0].Href.replace(tempProdUrl, baseUrl), {});
 		}).then(decisionResponse => {
-			return fetchApplicationDecision(decisionResponse.data.Links[0].Href.replace("https://quotationsearch.com/", baseUrl), 10);
+			console.log("Successfully received decition create response");
+			return fetchApplicationDecision(decisionResponse.data.Links[0].Href.replace(tempProdUrl, baseUrl), 10);
 		}).then(applicationDecision => {
 			callback(null, applicationDecision);
 		}).catch(err => {
@@ -27,15 +23,14 @@ exports.handler = function (event, context, callback) {
 }
 
 async function fetchApplicationDecision(url, maxRetries) {
-	console.log("Feteching applicationd decision from", url);
+	console.log("Feteching applicationd decision from", url, "with", maxRetries, "max retries");
 	let currentRetries = 0;
-	while (currentRetries < maxRetries) {
+	while (currentRetries++ < maxRetries) {
 		console.log("Retrying for the", currentRetries, "time");
 		let resp = await doDecisionCall(url, 10000);
 		if (resp.data.Status === "Approved") {
 			return resp.data;
 		}
-		currentRetries++;
 	}
 	throw new Error("Failed to retrieve decision after " + maxRetries + " retires");
 }
