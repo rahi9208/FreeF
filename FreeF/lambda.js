391b5cafc8d14a5e50d6e4b1eb209ee792f1bcce
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-07-20 11:49:33 +0530

    commit

--- FreeF/lambda.js
+++ FreeF/lambda.js
@@ -1,15 +1,50 @@
 let AWS = require('aws-sdk');
-let axios = require('axios');
+let axios = require('axios2');
 
 exports.handler = function (event, context, callback) {
 
 	//create application
 	axios.post("https://private-anon-1b3fb5589e-quotationsearchapi.apiary-mock.com/applications", event)
 		.then(resp => {
-			console.log("Successfully recieved response", resp);
-
+			console.log("Successfully recieved response", resp.data);
+			if (resp.data.Links && resp.data.Links.length > 0 && resp.data.Links[0].Href) {
+				console.log("Application created, Creating application decision...", resp.data.Links[0].Href);
+				callback(null, resp.data.Links[0].Href);
+				/*axios.post(resp.data.Links[0].Href, {}).then(resp => {
+					callback(null, resp.data);
+				}).catch(err=>{
+					callback(err);
+				});*/
+				return true;
+			} else {
+				throw new Error("Unexpected payload in step 1");
+			}
+		}).then(decisionResponse => {
+			console.log("Decision response recieved", decisionResponse.data);
+			callback(null, decisionResponse.data);
 		}).catch(err => {
 			console.error("Error in creating new pllication", err);
 			callback(err, null);
 		})
+}
+
+async function fetchApplicationDecision(url, maxRetries) {
+	let currentRetries = 0;
+	while (currentRetries < maxRetries) {
+		console.log("Retrying for the", currentRetries, "time");
+		let resp = await doDecisionCall(url, 10000);
+		if (resp.data.Status === "Approved") {
+			return resp.data.Status;
+		}
+		currentRetries++;
+	}
+	throw new Error("Failed to retrieve decision after " + maxRetries + " retires");
+}
+
+function doDecisionCall(url, delay) {
+	new Promise((resolve, reject) => {
+		setTimeout(() => {
+			axios.get(url).then(resolve).catch(reject);
+		}, delay);
+	});
 }
